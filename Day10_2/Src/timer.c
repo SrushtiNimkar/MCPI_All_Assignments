ed1073900fcc93f1d657e5854dcd526c7fd14ab6
E2_83842_Srushti <srustinimkar2001@gmail.com>
2024-04-05 17:11:58 +0530

    Day10_2

--- Day10_2/Src/timer.c
+++ Day10_2/Src/timer.c
@@ -2,33 +2,33 @@
  * timer.c
  *
  *  Created on: Apr 4, 2024
- *      Author: Nilesh
+ *      Author: Ghanshyam
  */
 
 #include "timer.h"
 
 void TimerInit(void) {
 	// enable timer clock
-	RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
+	RCC->APB1ENR |= RCC_APB1ENR_TIM7EN;
 	// set prescalar
-	TIM6->PSC = TIM_PR-1;
+	TIM7->PSC = TIM_PR-1;
 }
 
 void TimerDelayMs(uint32_t ms) {
 	// set number of clocks to count
 	uint16_t cnt = (TCLK / 1000) * ms / TIM_PR;
-	TIM6->ARR = cnt - 1;
+	TIM7->ARR = cnt - 1;
 	// start count from 0
-	TIM6->CNT = 0;
+	TIM7->CNT = 0;
 	// start timer counter
-	TIM6->CR1 |= TIM_CR1_CEN;
+	TIM7->CR1 |= TIM_CR1_CEN;
 	// wait for update interrupt flag
-	while(! (TIM6->SR & TIM_SR_UIF))
+	while(! (TIM7->SR & TIM_SR_UIF))
 		;
 	// clear the update interrupt flag
-	TIM6->SR &= ~TIM_SR_UIF;
+	TIM7->SR &= ~TIM_SR_UIF;
 	// stop timer counter
-	TIM6->CR1 &= ~TIM_CR1_CEN;
+	TIM7->CR1 &= ~TIM_CR1_CEN;
 }
 
 
